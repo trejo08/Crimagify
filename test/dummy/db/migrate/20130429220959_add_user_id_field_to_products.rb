1450971d05d7a483f0b0481da4bef51fe3e084e3
Juan Trejo <trejo_cola@hotmail.com>
2013-05-02 08:44:36 -0600

    borrar migracion

--- test/dummy/db/migrate/20130429220959_add_user_id_field_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdFieldToProducts < ActiveRecord::Migration
-  def change
-  	add_column :products, :user_id, :integer, :default => 0, :null => false
-  	add_index :products, :user_id
-  end
-end
