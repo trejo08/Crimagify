0516b754039e528f1bf55abe9027d1787fb8957f
Juan Trejo <trejo_cola@hotmail.com>
2013-06-20 11:27:26 -0600

    fixing bug on initializer

--- config/initializers/load_variables.rb
+++ config/initializers/load_variables.rb
@@ -1,5 +1,5 @@
 CRIMAGIFY_ENV = {}
-env_file = File.join(Rails.root, 'config', 'billify.yml')
+env_file = File.join(Rails.root, 'config', 'crimagify_versions.yml')
 if File.exists?(env_file)
 	yml = YAML.load_file(env_file)
 	CRIMAGIFY_ENV = yml
